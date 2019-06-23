f196fdf1797a48e03383941d4175a2d86db34474
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-06-24 00:17:35 +0200

    Add another reefence / alternative to disable wake script

--- scripts/disable_wake.ps1
+++ scripts/disable_wake.ps1
@@ -1,4 +1,6 @@
 # https://superuser.com/questions/973009/conclusively-stop-wake-timers-from-waking-windows-10-desktop
+# See also: https://github.com/seagull/disable-automaticrestarts
+
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" `"$args`"" -Verb RunAs; exit }
 
 # disable wake for enabled scheduled tasks that are allowed to wake
