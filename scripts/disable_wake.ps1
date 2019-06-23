222255cf8cd2637ccb1097b048437a18083e45fd
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-06-24 00:30:38 +0200

    Add disable Reboot task

--- scripts/disable_wake.ps1
+++ scripts/disable_wake.ps1
@@ -3,25 +3,34 @@
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" `"$args`"" -Verb RunAs; exit }
 
+# disable Reboot task
+if (Get-ScheduledTask | ?{ $_.TaskName -eq "Reboot" -and $_.State -ne 'Disabled' }) {
+  # https://community.spiceworks.com/topic/2107576-windows-10-what-is-update-orchestrator-service?page=1#entry-7673299
+  # https://superuser.com/a/1016445/311688
+  takeown /R /F "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator";
+  icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot" /grant BUILTIN\Administrators:F
+  Disable-ScheduledTask -TaskName "Reboot" -TaskPath "\Microsoft\Windows\UpdateOrchestrator";
+}
+
 # disable wake for enabled scheduled tasks that are allowed to wake
 Get-ScheduledTask |
 ?{ $_.Settings.WakeToRun -eq $true -and $_.State -ne 'Disabled' } |
 %{
-    write-host $_
-    $_.Settings.WakeToRun = $false;
-    # https://community.spiceworks.com/topic/2107576-windows-10-what-is-update-orchestrator-service?page=1#entry-7673299
-    # https://superuser.com/a/1016445/311688
-    takeown /R /F "C:\Windows\System32\Tasks$($_.TaskPath)$($_.TaskName)";
-    icacls "C:\Windows\System32\Tasks$($_.TaskPath)$($_.TaskName)" /grant BUILTIN\Administrators:F
-    Set-ScheduledTask -TaskName $_.TaskName -TaskPath $_.TaskPath -Settings $_.Settings
+  write-host $_
+  $_.Settings.WakeToRun = $false;
+  # https://community.spiceworks.com/topic/2107576-windows-10-what-is-update-orchestrator-service?page=1#entry-7673299
+  # https://superuser.com/a/1016445/311688
+  takeown /R /F "C:\Windows\System32\Tasks$($_.TaskPath)";
+  icacls "C:\Windows\System32\Tasks$($_.TaskPath)$($_.TaskName)" /grant BUILTIN\Administrators:F
+  Set-ScheduledTask -TaskName $_.TaskName -TaskPath $_.TaskPath -Settings $_.Settings
 }
 
 # disable wake for devices that are allowed to wake (list of wake capable devices: powercfg -devicequery wake_from_any)
 powercfg -devicequery wake_armed |
 %{
-    write-host $_
-    if ($_ -notmatch '^(NONE)?$')
-    { powercfg -devicedisablewake $_ }
+  write-host $_
+  if ($_ -notmatch '^(NONE)?$')
+  { powercfg -devicedisablewake $_ }
 }
 
 Write-Host "`n`nPress any key to exit script ...";
