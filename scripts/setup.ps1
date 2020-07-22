9dbe5b7735de2bbbe6b1f7240545360231f378e8
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-07-22 16:42:53 +0200

    Add step to setup BitLocker

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,5 +1,7 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" `"$args`"" -Verb RunAs; exit }
 
+# Setup BitLocker
+
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
