b6abdd3934dc402719a070b92657f327b8da0993
Csaba Apagyi <csaba.apagyi@gmail.com>
2021-05-15 19:37:32 +0200

    Remove virtual dev stuff and zoom

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -9,7 +9,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install googlechrome vlc spotify dropbox putty.install qbittorrent steam firefox nordvpn -y
 
 # Work stuff
-choco install zoom slack -y
+choco install slack -y
 
 # Dev stuff
 choco install visualstudiocode git.install make powershell-core microsoft-windows-terminal docker-desktop -y
@@ -34,28 +34,6 @@ cat ~/.ssh/id_rsa.pub
 # Refresh env at the end so you can use installations w/o terminal restart
 # Nevertheless restart might still be required at this point ("The term 'xyz' is not recognized")
 refreshenv
-
-# Virtual dev stuff
-# -------------------
-choco install virtualbox vagrant -y
-# -------------------
-
-# Ruby dev stuff
-# -------------------
-# Setup devkit for 2.4+
-# See also:
-#   - https://chocolatey.org/packages/msys2
-#   - https://github.com/oneclick/rubyinstaller2/wiki/FAQ
-choco install ruby msys2 -y
-refreshenv
-powershell -executionpolicy bypass ridk install 2 3
-
-# https://github.com/rubygems/rubygems/issues/2805
-"gem: --no-document --platform=ruby" | Set-Content -Encoding utf8 $HOME\.gemrc
-
-gem install bundler
-bundle config --global jobs 3
-# -------------------
 
 Write-Host "`n`nPress any key to exit script ...";
 $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") > $null; 
